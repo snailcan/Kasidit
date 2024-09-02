16806e42feace11546238922fccacbb2add96810
snailcan <gtogto321@hotmail.com>
2024-09-01 22:32:48 -0700

    sc

--- src/module.h
+++ src/module.h
@@ -16,9 +16,40 @@ WiFi 3 Mode
 #include<AsyncTCP.h>
 #include<ESPAsyncWebServer.h>
 #include<ESPmDNS.h>
+#include<map>
 
 /*---------------------- End Lid -----------------------*/
 /*---------------------- Start Variable -----------------------*/
+extern IPAddress ip;
+extern IPAddress gateways;
+extern IPAddress subnets;
+extern IPAddress dns;
+
+extern std::map<String, String> env_vars;
+
+extern String MODES;
+extern String AP_SSID;
+extern String AP_PSK;
+extern String STA_SSID;
+extern String STA_PSK;
+extern String mqtt_enable;
+extern String mqtt_broker;
+extern String mqtt_port;
+extern String mqtt_topic;
+extern String mqtt_username;
+extern String mqtt_password;
+extern String mqtt_interval;
+extern String RTU_enable;
+extern String RTU_deviceID;
+extern String RTU_baudRate;
+extern String TCP_wifi_enable;
+extern String TCP_eth_enable;
+extern String local_IP;
+extern String gateway;
+extern String subnet;
+extern String dnss;
+
+
 extern const char* ssid;
 extern const char* password;
 extern AsyncWebServer server;
@@ -30,5 +61,10 @@ void setup_wifi();
 void setup_wifiAP();
 void ConfigServer();
 void handleIndex(AsyncWebServerRequest *request);
+void writeEnvFile();
+void readEnvFile();
+void updateEnvVariable(const String &key, const String &value);
+bool configureNetwork(String IPAddressStr, String gatewayStr, String subnetStr, String dnsa);
+IPAddress stringToIPAddress(String ipStr);
 /*---------------------- End Function -----------------------*/
 #endif
